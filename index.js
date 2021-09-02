b98524dc2bd551d3bd5be430d54c723d8906c564
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-02 05:15:56 -0500

    commit tree 75fa266766d3e71e96faf8f2db0bb7d6eff5d6a3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+const superbowlWin = record => {
+  let yrResultObjWithAWin = record.find(yrResultObj => yrResultObj.result === 'W');
+  if (yrResultObjWithAWin === undefined) {
+    return undefined;
+  }
+  return yrResultObjWithAWin.year;
+};
